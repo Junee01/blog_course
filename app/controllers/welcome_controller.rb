b099baeecd5df84beace27bcc31ce12817378336
Sangjun Park <potter777777@naver.com>
2016-01-26 16:26:37 +0900

    Setup & styled basic project func

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
 	def index
 		#최근 3개만 발행 순서로 표시
 		@posts = Post.all.limit(3).order("created_at desc")
+		@projects = Project.all.limit(3).order("created_at desc")
 	end
 end
